a1e611bfd1e8626b8881b41df237cf8b5cf4e840
Giuseppe-Pallazini <pepeteste1883@gmail.com>
2024-06-14 12:01:35 -0300

    Mudança na rota principal '/'

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import LandPage from './pages/landingPage/index';
-
 import RobsonVolkcane from './pages/ClienteRobsonVolkcane/index'
 
 export default function Index() {
@@ -9,8 +7,7 @@ export default function Index() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/robson-volkcane' element={<RobsonVolkcane />} />
-                <Route path='/' element={<LandPage />} />
+                <Route path='/' element={<RobsonVolkcane />} />
             </Routes>
         </BrowserRouter>
     );
